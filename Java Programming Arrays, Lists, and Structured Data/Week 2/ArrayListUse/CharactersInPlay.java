92d0a8ed1944ad3a116918464d1fff315b6f3ec5
Eyvaz27 <eyvaz.najafli@hotmail.com>
2022-09-01 20:16:57 +0400

    09/01/2022

---
+++ Java Programming Arrays, Lists, and Structured Data/Week 2/ArrayListUse/CharactersInPlay.java
@@ -0,0 +1,66 @@
+
+/**
+ * Write a description of CharactersInPlay here.
+ * 
+ * @author Eyvaz Najafli
+ * @version 08/30/2022
+ */
+import edu.duke.*;
+import java.util.*;
+
+public class CharactersInPlay {
+    private ArrayList<String> Characters;
+    private ArrayList<Integer> Frequency;
+    public CharactersInPlay() {
+        Characters = new ArrayList<String>();
+        Frequency = new ArrayList<Integer>();
+    }
+    private void update(String person) {
+        int personIndex = Characters.indexOf(person);
+        if(personIndex == -1){
+            Characters.add(person);
+            Frequency.add(1);
+        }
+        else {
+            int value = Frequency.get(personIndex);
+            Frequency.set(personIndex, value + 1);
+        }
+    }
+    public void findAllCharacters(FileResource fr){
+        for (String line: fr.lines()) {
+            int placeDot = line.indexOf(".");
+            if(placeDot != -1){
+                String name = line.substring(0, placeDot);
+                if(name == name.toUpperCase()){
+                    update(name);
+                    //System.out.println(line);
+                }
+            }
+        }
+    }
+    public void charactersWithNumParts(int num1, int num2) {
+        if(num1 > num2)
+            System.out.println("### Invalid Input ###");
+        else {
+            int maxValue = 0;
+            String mostFreqCharacter = "None";
+            for (int k=0; k<Characters.size(); k++){
+                int currFreq = Frequency.get(k);
+                String currName = Characters.get(k);
+                if (currFreq>num1 && currFreq<num2)
+                    System.out.println(currFreq + "\t" + currName);
+                if(currFreq > maxValue){
+                    maxValue = currFreq;
+                    mostFreqCharacter = currName;
+                }
+            }
+            System.out.println("\nMost talking Character is: " + 
+                                mostFreqCharacter + " " + maxValue);
+        }
+    }
+    public void tester() {
+        FileResource fr = new FileResource();
+        findAllCharacters(fr);
+        charactersWithNumParts(20, 200);
+    }
+}
--- Java Programming Arrays, Lists, and Structured Data/Week 2/ArrayListUse/WordFrequencies.java
+++ Java Programming Arrays, Lists, and Structured Data/Week 2/ArrayListUse/WordFrequencies.java
@@ -5,6 +5,61 @@
  * @author Eyvaz Najafli
  * @version 08/28/2022
  */
-public class WordFrequencies {
+import edu.duke.*;
+import java.util.ArrayList;
 
+public class WordFrequencies {
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+    
+    public WordFrequencies() {
+        myWords = new ArrayList<String>();
+        myFreqs = new ArrayList<Integer>();
+    }
+    public void findUnique() {
+        myWords.clear();
+        myFreqs.clear();
+        FileResource fr = new FileResource();
+        
+        for (String word: fr.words()) {
+            // convert string to lower case
+            word = word.toLowerCase();
+            // remove extra punctuation characters
+            word = word.replaceAll("\\p{Punct}","");
+            int index = myWords.indexOf(word);
+            if(index == -1) {
+                myWords.add(word);
+                myFreqs.add(1);
+            }
+            else {
+                int value = myFreqs.get(index);
+                myFreqs.set(index, value+1);
+            }
+            
+        }
+    }
+    public void findIndexOfMax () {
+    }
+    public void tester() {
+        // relevant parameter initialization
+        int max_value = 0;
+        String mostFreqWord = "None";
+        // find unique words
+        findUnique();
+        // print detailed information
+        System.out.println("# Unique Words: " + myWords.size());
+        for (int k = 0; k < myWords.size(); k++) {
+            int currFreq = myFreqs.get(k);
+            String currWord = myWords.get(k);
+            System.out.println(currFreq + "\t" + currWord);
+            // find word with highest frequency
+            if(currFreq > max_value) {
+                max_value = currFreq;
+                mostFreqWord = currWord;
+            }                   
+        }
+        System.out.println("The word that occurs most " + 
+                           "often and its count are: " + 
+                           mostFreqWord + " " + max_value);
+    }
 }
