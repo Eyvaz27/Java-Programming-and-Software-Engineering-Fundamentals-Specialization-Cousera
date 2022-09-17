c0e99e415c59119b007c9c9a45f6d8cd36c0faf6
Eyvaz27 <eyvaz.necefli.std@bhos.edu.az>
2022-09-17 10:47:43 +0400

    09/17/2022 Minor Changes

--- Java Programming Arrays, Lists, and Structured Data/Week 3/WebLogProgram/Tester.java
+++ Java Programming Arrays, Lists, and Structured Data/Week 3/WebLogProgram/Tester.java
@@ -27,23 +27,23 @@ public class Tester
         System.out.println("There are "+ uniqueIPs+" IPs");
         System.out.println("Most Busy Day - "+
                            test.dayWithMostIPVisits(dayAnalysis));
-        ArrayList<String> mostVisitingIPs = test.iPsWithMostVisitsOnDay(dayAnalysis, "Sep 30");
-        System.out.println("Most Active Users -- "+mostVisitingIPs);
+        ArrayList<String> mostVisitingIPs = test.iPsWithMostVisitsOnDay(dayAnalysis, "Sep 29");
+        System.out.println("Most Active Users on Sep 29 -- "+mostVisitingIPs);
         HashMap<String, Integer> counts = test.countVisitsPerIP();
         System.out.println("Max Unique Visits = "+
                            test.mostNumberVisitsByIP(counts));
         System.out.println("Most Active Users :\n"+
                            test.iPsMostVisits(counts));
         System.out.println("Unique Users :\n"+counts);
-        test.printAll();
-        System.out.println("Unique Visits on Sep 14 = "+
-                           test.uniqueIPVisitsOnDay("Sep 14").size());
-        System.out.println("Unique Visits on Sep 30 = "+
-                           test.uniqueIPVisitsOnDay("Sep 30").size());
+        //test.printAll();
+        System.out.println("Unique Visits on Sep 24 = "+
+                           test.uniqueIPVisitsOnDay("Sep 24").size());
+        System.out.println("Unique Visits on Sep 27 = "+
+                           test.uniqueIPVisitsOnDay("Sep 27").size());
         System.out.println("Unique IPs in range 200-299 = "+
                            test.countUniqueIPsInRange(200, 299));
-        System.out.println("Unique IPs in range 300-399 = "+
-                           test.countUniqueIPsInRange(300, 399));      
+        System.out.println("Unique IPs in range 400-499 = "+
+                           test.countUniqueIPsInRange(400, 499));      
         //int num = 0;
         //System.out.println("Printing Valus Higher than "+num);
         //test.printAllHigherThanNum(num);
