dd9c9667c6650fccf000999db922ae051c0e55ec
Eyvaz27 <eyvaz.necefli.std@bhos.edu.az>
2022-10-03 12:01:01 +0400

    10/03/2022 get n closest Earth Quakes to Jakarta :)

--- Java Programming Principles of Software Design/Week 1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ Java Programming Principles of Software Design/Week 1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -12,31 +12,36 @@ public class ClosestQuakes {
     // need to continue from here and you should update the 
     // "addQuake" function to work smoothly
     public ArrayList<QuakeEntry> addQuake(ArrayList<QuakeEntry> quakeData, QuakeEntry newQuake, Location reference, int howMany){
-        int insertionIndex = 0;
-        int elementCount = 0;
-        // Initial Element count check 
-        if(quakeData.size()==0){
-            quakeData.add(newQuake);
-            return quakeData;
-        }
+        boolean insertionFlag = false;
+        int insertionIndex = howMany+100;
+        int elementCount = howMany;
         Location newLocation = newQuake.getLocation();
         for(int index=0; index<quakeData.size(); index++){
             QuakeEntry currentQuake = quakeData.get(index); 
             Location currentLoc = currentQuake.getLocation();
             if(currentLoc.distanceTo(reference)>newLocation.distanceTo(reference)){
-                    insertionIndex = index;
-                    break;
+                insertionIndex = index;
+                insertionFlag = true;
+                break;
             }  
         }
-        if(quakeData.size()<howMany){
-            elementCount = quakeData.size();
-            quakeData.add(quakeData.get(elementCount-1));
+        if(insertionFlag==true){
+            if(quakeData.size()<howMany){
+                elementCount = quakeData.size();
+                quakeData.add(quakeData.get(elementCount-1));
+            }
+            for(int index=elementCount-1; index>insertionIndex; index--){
+                QuakeEntry previousQuake = quakeData.get(index-1);
+                quakeData.set(index, previousQuake);
+            }
+            quakeData.set(insertionIndex, newQuake);
         }
-        for(int index=elementCount-1; index>insertionIndex; index--){
-            QuakeEntry previousQuake = quakeData.get(index-1);
-            quakeData.set(index, previousQuake);
+        else{
+            if(quakeData.size()<howMany){
+                // Initial Element addition 
+                quakeData.add(newQuake);
+            }
         }
-        quakeData.set(insertionIndex, newQuake);
         return quakeData;
     }
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location reference, int howMany) {
@@ -59,14 +64,14 @@ public class ClosestQuakes {
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        //String source = "data/nov20quakedata.atom";
-        String source = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
-
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        System.out.println("The most closest Earth Quakes to Jakarta:");
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);      
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
