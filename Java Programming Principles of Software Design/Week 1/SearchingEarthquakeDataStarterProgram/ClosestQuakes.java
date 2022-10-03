77ee9e9eff8a62a9b1ab7006bcb6425249cb4975
Eyvaz27 <eyvaz.necefli.std@bhos.edu.az>
2022-10-03 12:17:10 +0400

    10/03/2022 Searching EarthQuake Data complete

--- Java Programming Principles of Software Design/Week 1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ Java Programming Principles of Software Design/Week 1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -12,6 +12,8 @@ public class ClosestQuakes {
     // need to continue from here and you should update the 
     // "addQuake" function to work smoothly
     public ArrayList<QuakeEntry> addQuake(ArrayList<QuakeEntry> quakeData, QuakeEntry newQuake, Location reference, int howMany){
+        // This function applies kind of Insertion sort in order
+        // to select the most closest earthQuakes to reference location 
         boolean insertionFlag = false;
         int insertionIndex = howMany+100;
         int elementCount = howMany;
@@ -46,22 +48,45 @@ public class ClosestQuakes {
     }
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location reference, int howMany) {
         ArrayList<QuakeEntry> quakesClosest = new ArrayList<QuakeEntry>();
-        // TO DO
-        // you need to continue from here 
-        // add elements to the list as in the case of
-        // insertion sort in order to maintain consistency
         int elementCount = howMany;
         if(quakeData.size()<howMany){
             elementCount = quakeData.size();
         }
-        for(int k=1; k<quakeData.size(); k++){
+        for(int k=0; k<quakeData.size(); k++){
             QuakeEntry currentQuake = quakeData.get(k); 
             quakesClosest = addQuake(quakesClosest, currentQuake, 
                                      reference, elementCount);
         }
         return quakesClosest;
     }
-
+    
+    public int indexOfLargest(ArrayList<QuakeEntry> quakeData) {
+        int indexLargest = 100;
+        double maxMagnitude = 0;
+        for (int index=0; index<quakeData.size(); index++){
+            double currMagnitude = quakeData.get(index).getMagnitude();
+            if(currMagnitude > maxMagnitude){
+                maxMagnitude = currMagnitude;
+                indexLargest = index;
+            }
+        }
+        return indexLargest;
+    }
+    
+    public ArrayList<QuakeEntry> getLargest(ArrayList<QuakeEntry> quakeData, int howMany) {
+        ArrayList<QuakeEntry> quakesLargest = new ArrayList<QuakeEntry>();
+        int elementCount = howMany;
+        if(quakeData.size()<howMany){
+            elementCount = quakeData.size();
+        }
+        for(int k=0; k<elementCount; k++){
+            int indexLargest = indexOfLargest(quakeData);
+            quakesLargest.add(quakeData.get(indexLargest));
+            quakeData.remove(indexLargest);
+        }
+        return quakesLargest;
+    }
+    
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
         String source = "data/nov20quakedatasmall.atom";
@@ -80,4 +105,18 @@ public class ClosestQuakes {
         System.out.println("number found: "+close.size());
     }
     
+    public void findLargestQuakes () {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for "+list.size());
+
+        ArrayList<QuakeEntry> largestQuakes = getLargest(list, 5);      
+        for(int k=0; k < largestQuakes.size(); k++){
+            QuakeEntry entry = largestQuakes.get(k);
+            System.out.println(entry);
+        }
+        System.out.println("number found: "+largestQuakes.size());
+    }
 }
